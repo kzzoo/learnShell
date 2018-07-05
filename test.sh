9df1e8d0c27cf8869b8a6728e16858bbc59b7afe
kzz <kzzoo@qq,com>
2018-07-05 23:55:00 +0800

    fifth test

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+:<<! 
 echo "Hello World!"
 your_name="tom"
 echo $your_name
@@ -41,4 +42,10 @@ sdfsdf
 sdf
 !
 EOF
+!
 
+echo "shell 传递参数实例！"
+echo "执行的文件名：$0"
+echo "第一个参数为：$1"
+echo "第二个参数为：$2"
+echo "第三个参数为：$3"
