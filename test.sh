511b05ff09e96b1e5195d7a48b9021c03b0fea61
zhangkun <zhangkun0702@thundersoft.com>
2018-07-05 18:31:13 +0800

    fourth test.sh

--- test.sh
+++ test.sh
@@ -11,3 +11,34 @@ str='this is a string'
 your_name='qinjx'
 str="Hello,I know you are \"${your_name}\"!" 
 echo ${str}
+#get the length of string
+string="abcd"
+echo ${#string} 
+#find child string
+string="runoob is a great company"
+echo `expr index "${string}" is `
+#define and read array
+array1=(value0 value1 value2 value3)
+echo ${array1[2]}
+echo ${array1[@]}
+#get the number of array
+length=${#array1[@]}
+echo ${length}
+#get the length of ele of arr
+length=${#array1[2]}
+echo ${length}
+
+:<<EOF
+多行注释
+:<<'
+asdz
+asdf
+aweq
+'
+:<<!
+dfads
+sdfsdf
+sdf
+!
+EOF
+
