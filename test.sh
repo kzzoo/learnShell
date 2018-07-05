ff3b8737d968bbc438fc00d185f85ace586db2eb
zhangkun <zhangkun0702@thundersoft.com>
2018-07-05 18:10:56 +0800

    third commit

--- test.sh
+++ test.sh
@@ -3,4 +3,11 @@ echo "Hello World!"
 your_name="tom"
 echo $your_name
 your_name="alibaba"
-echo $your_name
+echo ${your_name}
+myUrl="www.w3school.com"
+readonly myUrl
+unset your_name
+str='this is a string'
+your_name='qinjx'
+str="Hello,I know you are \"${your_name}\"!" 
+echo ${str}
